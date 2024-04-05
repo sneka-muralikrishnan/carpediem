a077e4dc991771f1530e9b4b87c547bf025b2495
sairammr <sairam1203mr@gmail.com>
2024-04-05 22:53:29 +0530

    login page update

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
 app = Flask(__name__)
+user = {
+    "sairam":12345,
+        "sneka":12345,
+            "philo":12345,
+                "sai":12345,
+
+}
 @app.route("/")
 def index():
     return render_template("Welcome.html")
@@ -7,6 +14,27 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/image", methods=["POST"])
+def image():
+    username = request.form["username"]
+    password = request.form["password"]
+    try:
+        if username in user:
+            if password==12345:
+                print("done")
+                return render_template("/image.html")
+        else:
+            error = "Invalid username or password. Please try again."
+            return render_template("/login.html", error=error)
+    except:
+        return "error"
+        
+    
+@app.route("/imageupload")
+def imageupload():
+    return render_template("image.html")  
+        
 
 @app.route("/comment")
 def comment():
