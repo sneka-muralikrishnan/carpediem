661eed59e3e1ab760203f1fcaa5c228facadd6e5
sairammr <sairam1203mr@gmail.com>
2024-04-06 00:58:58 +0530

    pages added

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
+import os
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 user = {
     "sairam":12345,
-        "sneka":12345,
-            "philo":12345,
-                "sai":12345,
+    "sneka":12345,
+    "philo":12345,
+    "sai":12345,
 
 }
 @app.route("/")
@@ -20,13 +24,12 @@ def image():
     username = request.form["username"]
     password = request.form["password"]
     try:
-        if username in user:
-            if password==12345:
-                print("done")
-                return render_template("/image.html")
+        if username in user and user[username] == int(password):
+            print("done")
+            return redirect("/imageupload")
         else:
             error = "Invalid username or password. Please try again."
-            return render_template("/login.html", error=error)
+        return render_template("login.html", error=error)
     except:
         return "error"
         
@@ -34,11 +37,21 @@ def image():
 @app.route("/imageupload")
 def imageupload():
     return render_template("image.html")  
-        
+
+@app.route("/success")
+def success():
+    return render_template("success.html")        
 
 @app.route("/comment")
 def comment():
     return render_template("comment.html")
+
+@app.route("/posttt")
+def posttt():
+    return render_template("posttt.html")
+
+ 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
